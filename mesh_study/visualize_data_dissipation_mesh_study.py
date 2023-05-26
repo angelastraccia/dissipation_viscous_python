5349054ee40a79765ddc9acb5bc280f344fa9b7d
Angela <angelastraccia@gmail.com>
2023-05-26 12:47:46 -0700

    Create visualize_data_dissipation_mesh_study.py

---
+++ mesh_study/visualize_data_dissipation_mesh_study.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  8 09:45:11 2022
+
+@author: GALADRIEL_GUEST
+"""
+
+import scipy.io
+import scipy
+import math
+import pandas as pd
+import glob
+import csv
+import numpy as np
+import pickle
+import importlib
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import os
+import seaborn as sns
+import shutil
+
+os.chdir("N:/vasospasm/dissipation_viscous_calculation/python_viscous_dissipation")
+
+pd.options.display.float_format = '{:.2f}'.format
+
+
+# %% Functions & Main
+
+
+def load_dict(name):
+    """
+    Parameters
+    ----------
+    name : str. path + name of the dictionary one wants to load
+    Returns
+    -------
+    b : the loaded dictionary
+    """
+    with open(name + ".pkl", "rb") as handle:
+        b = pickle.load(handle)
+    return b
+
+
+def get_list_files_dat(pinfo, case, base_size, num_cycle):
+    """
+
+
+    Parameters
+    ----------
+    pinfo : str, patient information, composed of pt/vsp + number.
+    num_cycle : int, number of the cycle computed
+    case : str, baseline or vasospasm
+
+    Returns
+    -------
+    onlyfiles : list of .dat files for the current patient, for the case and cycle wanted.
+    """
+
+    num_cycle = str(num_cycle)
+
+    pathwd = "N:/vasospasm/mesh_independence_study/" + pinfo + "/" + case + "/base_size_" + base_size + "/3-computational/hyak_submit/"
+
+    os.chdir(pathwd)
+    onlyfiles = []
+    for file in glob.glob("*.dat"):
+        if pinfo + "_" + case + "_cycle" + num_cycle in file:
+            onlyfiles.append(file)
+    indices = [l[:-4] for l in onlyfiles]
+
+    return onlyfiles, indices,pathwd
+
+def calculate_dissipation(pinfo, case, base_size, i_vessel, ddissipation):
+
+    len_cycle = 30
+    num_cycle = 2
+    onlydat, data_indices, pathwd = get_list_files_dat(pinfo, case, base_size, num_cycle)
+    
+    dissipation_in_time = [ddissipation.get("{}".format(data_indices[k])).get("dissipation{}".format(i_vessel))[1]
+        for k in range(len_cycle)]
+    
+    # Dissipation in micro watts
+    dissipation = np.mean(dissipation_in_time)*1e6
+    
+    # Volume in mm^2
+    volume = ddissipation.get("{}".format(data_indices[0])).get(
+        "dissipation{}".format(i_vessel))[2]*1e6
+    
+    dissipation_per_volume_in_time = [ddissipation.get("{}".format(data_indices[k])).get(
+        "dissipation{}".format(i_vessel))[3]
+        for k in range(len_cycle)]
+
+    # Dissipation per volume in W/m^2
+    dissipation_per_volume = np.mean(dissipation_per_volume_in_time)
+
+    return dissipation, volume, dissipation_per_volume
+
+#%% Load data
+
+pinfo = input('Patient number -- ')
+base_size = input('Base size -- ')
+num_cycle = 2
+
+case = 'baseline'
+dissipation_path_baseline = "N:/vasospasm/mesh_independence_study/" + pinfo + "/" + case + "/base_size_" + base_size + "/4-results/dissipation_viscous/"
+centerline_data_path = "N:/vasospasm/mesh_independence_study/" + pinfo + "/" + case + "/"
+dpoints_bas = load_dict(centerline_data_path  + "points_" + pinfo + "_" + case)
+ddissipation_bas = load_dict(dissipation_path_baseline + "dissipation_" + pinfo + "_" + case)
+
+case = 'vasospasm'
+dissipation_path_vasospasm = "N:/vasospasm/mesh_independence_study/" + pinfo + "/" + case + "/base_size_" + base_size + "/4-results/dissipation_viscous/"
+ddissipation_vas = load_dict(dissipation_path_vasospasm + "dissipation_" + pinfo + "_" + case)
+
+#%% Remove existing directories and create new ones for figures
+# Save figures in both 4-results baseline and vasospasm directories
+figure_path_baseline = (dissipation_path_baseline+"figures/")
+figure_path_vasospasm = (dissipation_path_vasospasm + "figures/")
+
+# Save figures in both 4-results baseline and vasospasm directories
+if os.path.exists(figure_path_baseline):
+    shutil.rmtree(figure_path_baseline)
+if os.path.exists(figure_path_vasospasm):
+    shutil.rmtree(figure_path_vasospasm)
+
+if not os.path.exists(figure_path_baseline):
+    os.makedirs(figure_path_baseline)
+if not os.path.exists(figure_path_vasospasm):
+    os.makedirs(figure_path_vasospasm)
+#%%
+
+num_vessels = len(dpoints_bas)
+
+# Define a color map for percent difference in resistance
+cmap = cm.get_cmap('RdPu')
+N_colors = 10
+color_range = cmap(np.linspace(0,1,N_colors))
+percent_diff_min = 0
+percent_diff_max = 2000
+dissipation_percent_difference_range = np.linspace(percent_diff_min,percent_diff_max,N_colors)
+
+# Instantiate variables
+df_all_vessels = pd.DataFrame()
+df_colors_all_vessels = pd.DataFrame()
+dresistance_bas, dresistance_vas = {}, {}
+
+for i_vessel in range(num_vessels):
+    
+    vessel_name = dpoints_bas.get("points{}".format(i_vessel))[0]
+    
+    dissipation_bas, volume_bas, dissipation_per_volume_bas = calculate_dissipation(pinfo, 'baseline', base_size, i_vessel, ddissipation_bas)
+    dissipation_vas, volume_vas, dissipation_per_volume_vas = calculate_dissipation(pinfo, 'vasospasm', base_size, i_vessel, ddissipation_vas)
+    
+    percent_diff_dissipation = (dissipation_vas-dissipation_bas)/dissipation_bas*100
+    percent_diff_volume = (volume_vas-volume_bas)/volume_bas*100
+    percent_diff_dissipation_per_volume = (dissipation_per_volume_vas-dissipation_per_volume_bas)/dissipation_per_volume_bas*100
+
+    vessel_data = {
+        'viscous dissipation baseline': dissipation_bas,
+        'viscous dissipation vasospasm': dissipation_vas,
+        'viscous dissipation percent difference': percent_diff_dissipation,
+        'volume baseline': volume_bas,
+        'volume vasospasm': volume_vas,
+        'volume percent difference': percent_diff_volume,
+        'dissipation per volume baseline': dissipation_per_volume_bas,
+        'dissipation per volume vasospasm': dissipation_per_volume_vas,
+        'dissipation per volume percent difference': percent_diff_dissipation_per_volume,
+    }
+
+    df_vessel = pd.DataFrame(vessel_data, index=[vessel_name])
+    #df_vessel.loc()
+    df_all_vessels = pd.concat([df_all_vessels,df_vessel])
+    
+    # Determine which color is associated with that percent change
+    percent_change_difference = np.abs(
+        np.subtract(dissipation_percent_difference_range, percent_diff_dissipation_per_volume))
+    closest_color_index = np.argmin(percent_change_difference)
+    
+    # Convet it to the RGB value from [0,255]
+    color_vessel = np.round(np.multiply(color_range[closest_color_index,0:3],255))
+    
+    # Save color in data frame
+    color_data = {vessel_name: color_vessel}
+    
+    df_color_vessel = pd.DataFrame(color_data, index=['red','green','blue'])
+    #df_color_vessel.loc()
+    df_colors_all_vessels = pd.concat([df_colors_all_vessels, df_color_vessel],axis=1)
+
+#%% Plot heat map
+
+f,(ax1,ax2,ax3,ax4) = plt.subplots(1,4,figsize = (25,17))
+
+plt.suptitle("Dissipation heatmap for "+ pinfo, fontsize = 40)
+sns.set(font_scale=1.8)
+
+xlabels = ['baseline','vasospasm']
+
+sns.heatmap(df_all_vessels.loc[:,['viscous dissipation baseline','viscous dissipation vasospasm']],xticklabels=xlabels,annot = True,cmap =plt.cm.RdPu,fmt = '.0f',linewidth=0.5,ax=ax1)
+ax1.set_title('Dissipation [microW]',fontsize=30)   
+
+sns.heatmap(df_all_vessels.loc[:,['volume baseline','volume vasospasm']],xticklabels=xlabels,annot = True,cmap =plt.cm.RdPu,fmt = '.2f',linewidth=0.5,ax=ax2)
+ax2.set_yticks([])
+ax2.set_title('Volume [mm^3]',fontsize=30)
+
+sns.heatmap(df_all_vessels.loc[:,['dissipation per volume baseline','dissipation per volume vasospasm']],xticklabels=xlabels,annot = True,cmap =plt.cm.RdPu,linewidth=0.5,ax=ax3)
+ax3.set_yticks([])
+ax3.set_title('Dissipation per volume [W/m^3]',fontsize=30)
+
+sns.heatmap(df_all_vessels.loc[:,['viscous dissipation percent difference','volume percent difference','dissipation per volume percent difference']],xticklabels=['Dissipation','Volume','Dissipation per volume'],annot = True,fmt ='.0f',cmap =plt.cm.RdPu,linewidth=0.5,ax=ax4,vmin=percent_diff_min, vmax=percent_diff_max)
+for t in ax4.texts: t.set_text(t.get_text() + " %")
+ax4.set_title('Percent difference',fontsize=30)
+ax4.set_yticks([])
+
+plt.savefig(figure_path_baseline + "plot_heatmap_dissipation_" + pinfo + ".png")
+plt.savefig(figure_path_vasospasm + "plot_heatmap_dissipation_" + pinfo + ".png")
+
+# Export color data for percent change in resistance to CSV file
+
+df_colors_all_vessels.to_csv(dissipation_path_baseline + pinfo + "_colors_dissipation.csv")
+df_colors_all_vessels.to_csv(dissipation_path_vasospasm + pinfo + "_colors_dissipation.csv")
