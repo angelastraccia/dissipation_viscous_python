5707fad3a4aee8f25b05129c63fa40575445f828
Angela <angelastraccia@gmail.com>
2023-10-10 16:09:27 -0700

    Update main_plot_centerlines_vessel_segments.py

--- main_plot_centerlines_vessel_segments.py
+++ main_plot_centerlines_vessel_segments.py
@@ -14,6 +14,7 @@ import glob
 import time
 import scipy.io
 import scipy
+import matplotlib.pyplot as plt
 
 
 
@@ -82,56 +83,118 @@ def get_list_files_dat(pinfo, case, num_cycle):
 
 #%% Plot original points
 
-pinfo = input('Patient number -- ') 
-case = input ('Condition -- ')
+# pinfo = input('Patient number -- ') 
+# case = input ('Condition -- ')
 
-results_path = 'L:/vasospasm/' + pinfo + '/' + case + '/4-results/pressure_resistance/'        plot_spaced_points["vectors"] = downsampled_normal*0.001
+# patient_list = ['pt2', 'pt7','pt8','pt10','pt12','pt21','pt28',
+#                 'pt29','pt30','pt33','pt36','pt39','pt40',
+#                 'vsp4','vsp5','vsp6','vsp7','vsp16',
+#                 'vsp17','vsp19','vsp23','vsp24','vsp25','vsp26']
 
-fname_stl_m = 'L:/vasospasm/' + pinfo + '/' + case + \
-    '/1-geometry/' + pinfo + '_' + case + '_final.stl'
-stl_surf_m = pv.read(fname_stl_m)
+# condition_list = ["baseline","vasospasm"]
 
-dpoints_original = load_dict(results_path +"points_original_" + pinfo + "_" + case)
-dvectors_original = load_dict(results_path +"vectors_original_" + pinfo + "_" + case)
+patient_list = ['pt40']
+condition_list= ['baseline','vasospasm']
 
+for pinfo in patient_list:
+    for case in condition_list:
+        
+        print(pinfo + " " + case)
 
-dpoints_cleaned = load_dict(results_path + 'points_' + pinfo + '_' + case)
-dvectors_cleaned = load_dict(results_path + 'vectors_' + pinfo + '_' + case)
-
-#%%
-for i_vessel in range(len(dpoints_original)):
-
-    vessel_name = dpoints_original["points{}".format(i_vessel)][0]
-    # Plot STL and points
-    check_vessels_plot = pv.Plotter()
-    check_vessels_plot.add_mesh(stl_surf_m, opacity=0.3)
-
-    plot_original_points = pv.PolyData(
-        dpoints_original["points{}".format(i_vessel)][1])
-    check_vessels_plot.add_mesh(plot_original_points,label=vessel_name,color='w')
-    plot_original_points["vectors"] = dvectors_original["vectors{}".format(i_vessel)][1]*0.001
-    
-    plot_original_points.set_active_vectors("vectors")
-    check_vessels_plot.add_mesh(plot_original_points.arrows, lighting=False)
-    
-    plot_cleaned_points = pv.PolyData(
-        dpoints_cleaned["points{}".format(i_vessel)][1])
-    check_vessels_plot.add_mesh(plot_cleaned_points,label=vessel_name,color='k')
-    
-    check_vessels_plot.add_legend(size=(.2, .2), loc='upper right')
-    check_vessels_plot.show()
-    
-#%% Plot extracted regions
-
-results_path = "L:/vasospasm/" + pinfo + "/" + case + "/4-results/" + "dissipation_viscous/"
-
-# Read in indices corresponding to each region in the original mesh
-dcenterindices = load_dict(results_path +'centerpoint_indices_' + pinfo + '_' + case)
-
-# Get list of vessel names
-all_region_names = [dcenterindices.get("indices{}".format(i_region))[0] for i_region in range(0,len(dcenterindices))]
-num_regions = len(all_region_names)
-
+        pressure_path = 'L:/vasospasm/' + pinfo + '/' + case + '/4-results/pressure_resistance/'     
+        dissipation_path = "L:/vasospasm/" + pinfo + "/" + case + "/4-results/" + "dissipation_viscous/"   
+        
+        fname_stl_m = 'L:/vasospasm/' + pinfo + '/' + case + \
+            '/1-geometry/' + pinfo + '_' + case + '_final.stl'
+        stl_surf_m = pv.read(fname_stl_m)
+        
+        dpoints_original = load_dict(pressure_path +"points_original_" + pinfo + "_" + case)
+        dvectors_original = load_dict(pressure_path +"vectors_original_" + pinfo + "_" + case)
+        
+        
+        dpoints_cleaned = load_dict(pressure_path + 'points_' + pinfo + '_' + case)
+        dvectors_cleaned = load_dict(pressure_path + 'vectors_' + pinfo + '_' + case)
+        
+        
+        # Get list of filenames
+        i_data = 0
+        num_cycle = 2
+        onlyfiles, data_indices, pathwd = get_list_files_dat(pinfo,case,num_cycle)
+        data_filename = dissipation_path + data_indices[i_data] + '_epsilon.dat'
+        
+        # Read in Tecplot data file
+        tic = time.perf_counter()
+        reader = pv.get_reader(data_filename)
+        mesh_data_imported = reader.read()
+        mesh_data_final = mesh_data_imported[0]
+        print(mesh_data_final.cell_data)
+        toc = time.perf_counter()
+        time_minutes = (toc-tic)/60
+        print(f"Data import took {time_minutes:0.4f} minutes")
+        
+        
+        # Read in indices corresponding to each region in the original mesh
+        dcenterindices = load_dict(dissipation_path +'centerpoint_indices_' + pinfo + '_' + case)
+        
+        # #%%
+        # for i_vessel in range(len(dpoints_original)):
+        
+        #     vessel_name = dpoints_original["points{}".format(i_vessel)][0]
+        #     # Plot STL and points
+        #     check_vessels_plot = pv.Plotter()
+        #     check_vessels_plot.add_mesh(stl_surf_m, opacity=0.3)
+        
+        #     plot_original_points = pv.PolyData(
+        #         dpoints_original["points{}".format(i_vessel)][1])
+        #     check_vessels_plot.add_mesh(plot_original_points,label=vessel_name,color='w')
+        #     plot_original_points["vectors"] = dvectors_original["vectors{}".format(i_vessel)][1]*0.001
+            
+        #     plot_original_points.set_active_vectors("vectors")
+        #     check_vessels_plot.add_mesh(plot_original_points.arrows, lighting=False)
+            
+        #     plot_cleaned_points = pv.PolyData(
+        #         dpoints_cleaned["points{}".format(i_vessel)][1])
+        #     check_vessels_plot.add_mesh(plot_cleaned_points,label=vessel_name,color='k')
+            
+        #     check_vessels_plot.add_legend(size=(.2, .2), loc='upper right')
+        #     check_vessels_plot.show()
+            
+        #% Plot extracted regions
+        
+        # Get list of vessel names
+        all_vessel_names = [dcenterindices.get("indices{}".format(i_region))[0] for i_region in range(0,len(dcenterindices))]
+        num_vessels = len(all_vessel_names)
+        vessel_name_list = ['L_MCA','R_MCA','L_A1','L_A2','R_A1','R_A2',
+                            'L_PCOM','L_P1','L_P2','R_PCOM','R_P1','R_P2',
+                            'BAS','L_TICA','R_TICA']
+        
+        #Define a discretized color map
+        cmap = plt.get_cmap('jet')
+        colors = cmap(np.linspace(0, 1, num_vessels))
+        
+        plot_segments = pv.Plotter()
+        
+        plot_segments.add_mesh(stl_surf_m, opacity=0.3)
+        plot_segments.background_color = 'w'
+        
+        
+        # Cycle through each branch
+        for i_vessel in range(num_vessels):
+            
+            vessel_name = dcenterindices.get("indices{}".format(i_vessel))[0]
+            #print(vessel_name)
+            
+            if vessel_name in vessel_name_list:
+                branch_center_point_indices = dcenterindices.get("indices{}".format(i_vessel))[1]
+            
+                branch_segment_extracted = mesh_data_final.extract_cells(branch_center_point_indices)
+                
+                # Plot the branch colored by the branch ID
+                plot_segments.add_mesh(branch_segment_extracted, color=colors[i_vessel], label=vessel_name)
+        
+        plot_segments.add_legend(size=(.3, .3), loc='upper right',bcolor='w')
+        plot_segments.show()
+        
 
 
 
